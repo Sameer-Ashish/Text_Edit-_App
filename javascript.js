93d1b04267927e20eadc657d598df763cd4ae8e9
Sameer-Ashish <119566754+Sameer-Ashish@users.noreply.github.com>
2022-12-11 10:37:37 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function myFunction() {
-    document.getElementById("ss").innerHTML = "Hello Dolly!";
-    document.getElementById("sss").innerHTML = "How are you?";
-    document.getElementById("ll").innerHTML = "How are you?";
+    document.getElementById("ss").innerHTML = "Hello Dolly! w ef3f i43ygi3cfib w4iuf4c";
+    document.getElementById("sss").innerHTML ="How are you? n csbebiwdebbb bsai duiwbe";
+    document.getElementById("ll").innerHTML = "How are you? k ibbriuurbiuerv ububvwiv v4ug";
 
   }
